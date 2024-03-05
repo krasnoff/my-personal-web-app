c440c0fd307c5555e601a8a4caf549e6ae7c13ae
kobi krasnoff <krasnoff.kobi@gmail.com>
2024-03-05 22:12:31 +0200

    minor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
      
   try {
     postData = await getJSONData('./json/posts.json') as Posts[];
-    postData = postData.slice(0, 1);
+    postData = postData.slice(0, 2);
   } catch (err) {
 
   }
@@ -37,10 +37,10 @@ export default async function Home() {
               <div className="text-[1.375rem]">Recent Posts</div>
               <div><Link href="/blog" className="text-secondary">View all</Link></div>
             </div>
-            <div className="flex grow items-start">
+            <div className="flex grow items-start justify-between">
               {postData.map((post) => (
                 <div key={post.id} className="bg-white w-[26.125rem] h-[18.4375rem] rounded drop-shadow pt-[1.8rem] pb-[1.875rem] pl-[1rem] pr-[2.6875rem]">
-                  <div className="font-bold text-[1.625rem]">{post.title}</div>
+                  <div className="font-bold text-[1.625rem] line-clamp-2">{post.title}</div>
                   <div className=" pt-[1.2rem] pb-[1.2rem]">{post.publishDate.toString()}   |   {post.keyWords}</div>
                   <div className="line-clamp-4">
                     {post.text}
