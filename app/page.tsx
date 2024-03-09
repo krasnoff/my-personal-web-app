69496706593c47245f2352dad18d51df58a2068e
kobi krasnoff <krasnoff.kobi@gmail.com>
2024-03-09 16:28:44 +0200

    complete works display

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Posts } from "@/interfaces/posts";
+import { Projects } from "@/interfaces/projects";
 import { getJSONData } from "@/lib/posts";
 import Link from "next/link";
 
 export default async function Home() {
   let postData: Posts[] = [];
-  
+  let workData: Projects[] = [];
+
 
   const options: Intl.DateTimeFormatOptions  = {
     weekday: 'long',
@@ -20,6 +22,13 @@ export default async function Home() {
       const dateObj: Date = new Date(el.publishDate);
       el.publishDate = dateObj.toLocaleDateString("en-US", options);
     })
+  } catch (err) {
+
+  }
+
+  try {
+    workData = await getJSONData('./json/projects.json') as Projects[];
+    workData = workData.slice(0, 3);
   } catch (err) {
 
   }
@@ -66,19 +75,21 @@ export default async function Home() {
           <div className="mx-auto max-w-innerFrame flex items-stretch flex-col h-full pt-[2.125rem] pb-[3.0625rem]">
             <div className="text-[1.375rem]">Featured Works</div>
           </div>
-          <div className="flex grow mx-auto max-w-innerFrame pb-[0.875rem] mb-[1.9375rem] border-b">
-                <div className="w-[31.375rem]"><img src="/img/1_TSeWgZynzkbsF783uNeO3Q.webp" className="w-[31.375rem] rounded"></img></div>
-                <div className="pl-[1.125rem] grow-1">
-                  <div className="font-bold text-[1.875rem]">Designing Dashboards</div>
-                  <div className="flex py-[1.2rem]">
-                    <div className="bg-[#142850] flex text-white font-black text-[1.125rem] px-[0.6875rem] items-center rounded-full">2020</div>
-                    <div className="text-[1.25rem] text-light pl-[1.625rem]">Dashboard</div>
-                  </div>
-                  <div>
-                    Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
-                  </div>
+          {workData.map((workItem) => (
+            <div key={workItem.id} className="flex grow mx-auto max-w-innerFrame pb-[0.875rem] mb-[1.9375rem] border-b">
+              <div className="basis-1/5"><img src="/img/1_TSeWgZynzkbsF783uNeO3Q.webp" className="w-full rounded"></img></div>
+              <div className="basis-4/5 pl-[1.125rem]">
+                <div className="font-bold text-[1.875rem]">{workItem.title}</div>
+                <div className="flex py-[1.2rem]">
+                  <div className="bg-[#142850] flex text-white font-black text-[1.125rem] px-[0.6875rem] items-center rounded-full">{workItem.publishYear}</div>
+                  <div className="text-[1.25rem] text-light pl-[1.625rem]">{workItem.keywords}</div>
                 </div>
-          </div>
+                <div>
+                  {workItem.description}
+                </div>
+              </div>
+            </div>
+          ))}  
         </div>
       </>
   );
---
+++ interfaces/projects.ts
@@ -0,0 +1,9 @@
+export interface Projects {
+    id: number,
+    publishYear: number,
+    keywords: string,
+    title: string,
+    url: string,
+    gitUrl: string,
+    description: string
+}
