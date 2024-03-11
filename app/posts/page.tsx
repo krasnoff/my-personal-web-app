fa17eb66ffea98a32dff528e707d5a9c41de641b
kobi krasnoff <krasnoff.kobi@gmail.com>
2024-03-11 21:13:23 +0200

    complete works and post page

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,7 +1,37 @@
+import Post from "@/components/post";
+import { Posts } from "@/interfaces/posts";
+import { getJSONData } from "@/lib/posts";
+
 export default async function Posts() {
+    let postData: Posts[] = [];
+
+    const options: Intl.DateTimeFormatOptions  = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    };
+
+    try {
+        postData = await getJSONData('./json/posts.json') as Posts[];
+        postData.forEach(el => {
+          const dateObj: Date = new Date(el.publishDate);
+          el.publishDate = dateObj.toLocaleDateString("en-US", options);
+        })
+      } catch (err) {
+    
+    }
+    
     return (
         <>
-            <div>posts page</div>
+            <div className="mx-auto max-w-innerFrame font-bold text-5xl pl-4 mt-4">Posts</div>
+            <div className="mx-auto max-w-innerFrame flex items-stretch mt-4 flex-col">
+                {postData.map((post) => (
+                    <div key={post.id} className="bg-white pt-[1.8rem] pb-[1.875rem] pl-[1rem] pr-[2.6875rem] border-b border-Gray-600">
+                        <Post post={post}></Post>
+                    </div>
+                ))}
+            </div>
         </>
     );
 }
--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,7 +1,22 @@
+import Works from "@/components/work";
+import { Projects } from "@/interfaces/projects";
+import { getJSONData } from "@/lib/posts";
+
 export default async function Posts() {
+    let workData: Projects[] = [];
+
+    try {
+        workData = await getJSONData('./json/projects.json') as Projects[];
+    } catch (err) {
+    
+    }
+
     return (
         <>
-            <div>works page</div>
+            <div className="mx-auto max-w-innerFrame font-bold text-5xl mt-4 mb-8">Works</div>
+            {workData.map((workItem) => (
+                <Works workItem={workItem}></Works>
+            ))}
         </>
     );
 }
