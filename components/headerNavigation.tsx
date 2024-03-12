57a19c51ba9591a53dc513ce2ef04a74ecfd8adb
kobi krasnoff <krasnoff.kobi@gmail.com>
2024-03-12 22:33:09 +0200

    use hooks

--- components/headerNavigation.tsx
+++ components/headerNavigation.tsx
@@ -1,14 +1,17 @@
 'use client'
 
 import Link from "next/link";
+import { usePathname } from "next/navigation"; 
 
 export default function HeaderNavigation() {
+    const pathName = usePathname();
+    
     return (
         <>
             <div className="mx-auto max-w-outerFrame h-20 flex items-center justify-end font-medium">
-                <Link className="mr-8 cursor-pointer" href="/works">Works</Link>
-                <Link className="mr-8 cursor-pointer" href="/posts">Posts</Link>
-                <Link className="mr-8 cursor-pointer" href="/contact">Contact</Link>
+                <Link className={pathName === '/works' ? 'mr-8 cursor-pointer text-red-500' : 'mr-8 cursor-pointer'} href="/works">Works</Link>
+                <Link className={pathName === '/posts' ? 'mr-8 cursor-pointer text-red-500' : 'mr-8 cursor-pointer'} href="/posts">Posts</Link>
+                <Link className={pathName === '/contact' ? 'mr-8 cursor-pointer text-red-500' : 'mr-8 cursor-pointer'} href="/contact">Contact</Link>
             </div>
         </>
     );
