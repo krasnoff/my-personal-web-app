1a0660490ed3b8a12f0cda36f712d192368a31f8
kobi krasnoff <krasnoff.kobi@gmail.com>
2024-03-16 11:01:51 +0200

    complete detailed page

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,10 +1,46 @@
-export default function Page({ params }: { params: { id: string } }) {
+import { Posts } from "@/interfaces/posts";
+import { getJSONData } from "@/lib/posts";
+import Link from "next/link";
+
+export default async function Page({ params }: { params: { id: string } }) {
+    let postDataArr: Posts[] = [];
+    let postData: Posts | undefined;
+
+    const options: Intl.DateTimeFormatOptions  = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    };
+
+    try {
+        postDataArr = await getJSONData('./json/posts.json') as Posts[];
+        postData = postDataArr.find(el => el.id.toString() === params.id);
+    } catch (err) {
+
+    }
+
+    const publishDate =  (new Date((postData as Posts).publishDate)).toLocaleDateString("en-US", options);
+    
     return (
-        <>
-            <div className="mx-auto max-w-innerFrame font-bold text-5xl pl-4 mt-4">Posts</div>
-            <div className="mx-auto max-w-innerFrame flex items-stretch mt-4 flex-col">
-                data: {params.id}
-            </div>
-        </>
+        <div className="mx-auto max-w-innerFrame flex items-stretch mt-4 flex-col">
+            {postData && 
+                <>
+                    <div className="max-w-innerFrame font-bold text-4xl mt-4 mb-3">{postData.title}</div>
+                    <div className="mb-4">
+                        <div className="font-bold underline">Publish Date:</div>
+                        {publishDate}
+                    </div>
+                    <div className="mb-4">{postData.text}</div>
+                    <div className="mb-4">
+                        <div className="font-bold underline">Tech Stack:</div>
+                            {postData.keyWords}
+                        </div>
+                    <div>
+                        Link to the full article: <Link href={postData.url} target="_blank" className="text-blue-600 hover:underline">{postData.url}</Link>
+                    </div>
+                </>
+            }
+        </div>
     );
 }
--- app/works/[id]/page.tsx
+++ app/works/[id]/page.tsx
@@ -1,10 +1,36 @@
-export default function Page({ params }: { params: { id: string } }) {
+import { Projects } from "@/interfaces/projects";
+import { getJSONData } from "@/lib/posts";
+import Link from "next/link";
+
+export default async function Page({ params }: { params: { id: string } }) {
+    let workDataArr: Projects[];
+    let workData: Projects | undefined;
+
+    try {
+        workDataArr = await getJSONData('./json/projects.json') as Projects[];
+        workData = workDataArr.find(el => el.id.toString() === params.id);
+      } catch (err) {
+    
+    }
+    
     return (
-        <>
-            <div className="mx-auto max-w-innerFrame font-bold text-5xl pl-4 mt-4">Works</div>
-            <div className="mx-auto max-w-innerFrame flex items-stretch mt-4 flex-col">
-                data: {params.id}
-            </div>
-        </>
+        <div className="mx-auto max-w-innerFrame flex items-stretch mt-4 flex-col">
+            {workData && 
+                <>
+                    <div className="max-w-innerFrame font-bold text-4xl mt-4 mb-3">{workData.title}</div>
+                    <div className="mb-4">{workData.description}</div>
+                    <div className="mb-4">
+                        <div className="font-bold underline">Tech Stack:</div>
+                            {workData.keywords}
+                        </div>
+                    <div>
+                        Link to application site: <Link href={workData.url} target="_blank" className="text-blue-600 hover:underline">{workData.url}</Link>
+                    </div>
+                    <div>
+                        Link to GIT source code: <Link href={workData.gitUrl} target="_blank" className="text-blue-600 hover:underline">{workData.gitUrl}</Link>
+                    </div>
+                </>
+            }
+        </div>
     );
 }
