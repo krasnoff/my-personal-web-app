37857acf8996f39e6ddc113b07f93a5b48256909
kobi krasnoff <krasnoff.kobi@gmail.com>
2024-03-21 20:40:27 +0200

    minor fix 2

--- components/headerNavigation.tsx
+++ components/headerNavigation.tsx
@@ -9,9 +9,13 @@ export default function HeaderNavigation() {
     return (
         <>
             <div className="mx-auto max-w-outerFrame h-20 flex items-center justify-end font-medium">
-                <Link className={pathName.indexOf('/works') > -1 ? 'mr-8 cursor-pointer text-red-500' : 'mr-8 cursor-pointer'} href="/works">Works</Link>
-                <Link className={pathName.indexOf('/posts') > -1 ? 'mr-8 cursor-pointer text-red-500' : 'mr-8 cursor-pointer'} href="/posts">Posts</Link>
-                <Link className={pathName.indexOf('/contact') > -1 ? 'mr-8 cursor-pointer text-red-500' : 'mr-8 cursor-pointer'} href="/contact">Contact</Link>
+                {pathName ? 
+                    <>
+                        <Link className={pathName.indexOf('/works') > -1 ? 'mr-8 cursor-pointer text-red-500' : 'mr-8 cursor-pointer'} href="/works">Works</Link>
+                        <Link className={pathName.indexOf('/posts') > -1 ? 'mr-8 cursor-pointer text-red-500' : 'mr-8 cursor-pointer'} href="/posts">Posts</Link>
+                        <Link className={pathName.indexOf('/contact') > -1 ? 'mr-8 cursor-pointer text-red-500' : 'mr-8 cursor-pointer'} href="/contact">Contact</Link>)
+                    </>
+                : null}
             </div>
         </>
     );
