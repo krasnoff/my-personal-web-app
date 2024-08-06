3529e9148ed3801abc62f8a3d36d5e2f146ef42b
Kobi Krasnoff <krasnoff.kobi@gmail.com>
2024-08-06 20:05:57 +0300

    merge from master

--- components/headerNavigation.tsx
+++ components/headerNavigation.tsx
@@ -10,11 +10,11 @@ export default function HeaderNavigation() {
         <>
             <div className="mx-auto max-w-outerFrame h-20 flex items-center justify-end font-medium">
                 {pathName ? 
-                    <>
+                    <div>
                         <Link className={pathName.indexOf('/works') > -1 ? 'mr-8 cursor-pointer text-red-500' : 'mr-8 cursor-pointer'} href="/works">Works</Link>
                         <Link className={pathName.indexOf('/posts') > -1 ? 'mr-8 cursor-pointer text-red-500' : 'mr-8 cursor-pointer'} href="/posts">Posts</Link>
                         <Link className={pathName.indexOf('/contact') > -1 ? 'mr-8 cursor-pointer text-red-500' : 'mr-8 cursor-pointer'} href="/contact">Contact</Link>
-                    </>
+                    </div>
                 : null}
             </div>
         </>
