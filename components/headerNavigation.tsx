aebd0f38c2315c662d81fab5adea344bce2f0b2f
Kobi Krasnoff <krasnoff.kobi@gmail.com>
2024-08-06 20:27:23 +0300

    update link homepage

--- components/headerNavigation.tsx
+++ components/headerNavigation.tsx
@@ -2,6 +2,8 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation"; 
+import Image from 'next/image';
+import homeIcon from "../svg/home.svg";
 
 export default function HeaderNavigation() {
     const pathName = usePathname();
@@ -9,6 +11,16 @@ export default function HeaderNavigation() {
     return (
         <>
             <div className="mx-auto max-w-outerFrame h-20 flex items-center justify-end font-medium">
+                <div className="flex-1 ml-8">
+                <Link href="/">
+                    <Image
+                        priority
+                        src={homeIcon}
+                        alt="Goto homepage"
+                        className="w-[27px] h-[27px]"
+                    />
+                </Link>
+                </div>
                 {pathName ? 
                     <div>
                         <Link className={pathName.indexOf('/works') > -1 ? 'mr-8 cursor-pointer text-red-500' : 'mr-8 cursor-pointer'} href="/works">Works</Link>
