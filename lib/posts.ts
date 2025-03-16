697bb848eb2776f03639ec8c9b83ab2f8f74b24d
Kobi Krasnoff <krasnoff.kobi@gmail.com>
2025-03-16 21:03:44 +0200

    another minor fix

--- lib/posts.ts
+++ lib/posts.ts
@@ -42,7 +42,7 @@ const getWorks = () => {
             "title": "React Accessibility Component",
             "url": "https://www.npmjs.com/package/react-accessibility-component",
             "gitUrl": "https://github.com/krasnoff/vite-react-ts-component",
-            "description": "AccessibilityComponent is a React component designed to enhance web accessibility by providing essential features that support users with disabilities. It is intended to be used as part of a broader strategy for making web applications accessible."
+            "description": "AccessibilityComponent is a React component designed to enhance web accessibility by providing essential features that support users with disabilities. It is intended to be used as part of a broader strategy for making web applications accessible.",
             "image": "1_TSeWgZynzkbsF783uNeO3Q.webp"
         },
         {
